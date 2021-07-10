82d76958ce599c5823c2a9f78d1dcf173f7b1a01
brnfra <devlabbr@outlook.com>
2021-07-10 01:34:53 -0300

    update and fix

--- desafios_dio/busca_e_substituicao/desafio_strings/Main.java
+++ desafios_dio/busca_e_substituicao/desafio_strings/Main.java
@@ -1,38 +1,29 @@
 import java.io.*;
 
 public class Main {
-
-    //static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedReader in;
-
-    {
-        try {
-            in = new BufferedReader(new FileReader("desafios_dio/busca_e_substituicao/"+
-                    "desafio_strings/inputs.txt"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
     static PrintWriter out = new PrintWriter(System.out);
 
+    public static void main(String[] args) throws IOException {
+        //static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-
-    public static void main(String[] args) throws IOException,FileNotFoundException {
+        BufferedReader  in = new BufferedReader(new FileReader("desafios_dio/busca_e_substituicao/"+
+                "desafio_strings/inputs.txt"));
         String min, max, word1,word2  ; //declare suas variaveis aqui
         while ((   word1 = in.readLine()) != null) { // complete seu código
             word2 = in.readLine();
-//            if (  ) {
-//
-//            } else {
-//
-//            }
+            if (word2.length()>word1.length()  ) {
+                min = word1;
+                max = word2;
+            } else {
+                min = word2;
+                max = word1;
+            }
             int minLength = min.length();
             int maxS = minLength;
             boolean f = true;
             while (maxS > 0 && f) { //altere as variaveis, se achar necessario
                 int diff = minLength - maxS;
-                for (int i = 0; i <= ; ) { //complete o laco de repeticao
+                for (int i = 0; i <= diff ;i++ ) { //complete o laco de repeticao
                     if (max.contains(min.substring(i, i + maxS))) {
                         f = false;
                         maxS++;
